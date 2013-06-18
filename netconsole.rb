8d484a6fe655f4beb0881de3e72b3239a0fb9c69
evinugur <evinoog96@gmail.com>
2013-06-17 22:38:39 -0400

    wrote netconsole in ruby

---
+++ netconsole.rb
@@ -0,0 +1,37 @@
+require 'socket'
+require 'thread'
+#set this to your team number
+TEAM = "1073"
+#UDP ports for NetConsole
+IN = 6666
+OUT = 6668
+
+#function to print UDP packets from the cRio
+def receive
+	listener = UDPSocket.new
+	listener.bind(nil, IN)
+	while true do 
+		print listener.recvfrom(16)[0]
+	end
+end
+#function to send UDP 
+def send
+	sender = UDPSocket.new
+	while true do
+		input = gets.chomp
+		input << "\r\n"
+		sender.send(input, 0, teamIP, OUT)
+	end
+end
+
+t1 = Thread.new {receive}
+t2 = Thread.new {send}
+
+t1.join
+t2.join
+
+#returns IP address of the cRio
+def teamIP
+	return nil unless TEAM.match /\d\d\d\d/
+	ip = "10." << TEAM[0,2] << "." << TEAM[2,3] << ".2"
+end
---
+++ readme.md
@@ -0,0 +1,28 @@
+#FRC NetConsole for Ruby
+
+This is a port of WPI's NetConsole to Ruby for the [**FIRST Robotics Competition**]("usfirst.org")
+
+There's really nothing too cool to see here.
+
+Uses standard in for commands, find them [here](http://touro.ligo-la.caltech.edu/~cparames/CDS/vxWorks_commands.html)
+The script was written by [Evin Ugur](mailto:evinoog96@gmail.com)
+
+Read the **brief** documentation below to get started
+
+##Installation
+
+
+##Configuration
+Set the variable `TEAM` to your team number
+
+Like this
+
+```
+TEAM = 1073
+```
+
+It should be four digits, if you're less than 4 digits, prefix your number with zeroes. Team 161 would look like this
+
+```
+TEAM = 0161
+```
